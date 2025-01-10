6b67425373dd0517aa60003cb3dd0120da18d30b
fraperper5 <fraperper5@alum.us.es>
2025-01-10 12:11:42 +0100

    cambios

--- decide/census/admin.py
+++ decide/census/admin.py
@@ -6,7 +6,7 @@ from .models import Census
 class CensusAdmin(admin.ModelAdmin):
     list_display = ('voting_id', 'voter_id')
     list_filter = ('voting_id', )
-
+#Hola
     search_fields = ('voter_id', )
 
 
